7c4e865769e8336d1b32675849a95e7322e661aa
rey-soleil <68834278+rey-soleil@users.noreply.github.com>
2023-05-18 08:36:19 -0700

    feat(api): create openai object using api key

--- app/api/generate/route.ts
+++ app/api/generate/route.ts
@@ -1,3 +1,10 @@
+import { Configuration, OpenAIApi } from "openai";
+
+const configuration = new Configuration({
+  apiKey: process.env.OPENAI_API_KEY,
+});
+const openai = new OpenAIApi(configuration);
+
 export async function GET(request: Request) {
   return new Response("Hello world!", { status: 200 });
 }
