98b8468fc226c9c8af145508833f506bae93203d
rey-soleil <68834278+rey-soleil@users.noreply.github.com>
2023-05-19 11:21:10 -0700

    feat(input): implement form in UserInput

--- app/components/Messenger.tsx
+++ app/components/Messenger.tsx
@@ -1,11 +1,16 @@
+"use client";
+
+import { useState } from "react";
 import ChatHistory from "./ChatHistory";
 import UserInput from "./UserInput";
 
 export default function Messenger() {
+  const [userMessage, setUserMessage] = useState<string>("");
+
   return (
-    <div className="outline w-4/6 md:w-1/2 h-48 md:h-96 flex flex-col justify-between">
+    <div className="outline w-4/6 md:w-1/2 min-h-[100px] md:h-96 flex flex-col justify-between">
       <ChatHistory />
-      <UserInput />
+      <UserInput userMessage={userMessage} setUserMessage={setUserMessage} />
     </div>
   );
 }
--- app/components/UserInput.tsx
+++ app/components/UserInput.tsx
@@ -1,11 +1,30 @@
 "use client";
 
-export default function UserInput() {
+type UserInputProps = {
+  userMessage: string;
+  setUserMessage: React.Dispatch<React.SetStateAction<string>>;
+};
+
+export default function UserInput({
+  userMessage,
+  setUserMessage,
+}: UserInputProps) {
+  function onSubmit(event: React.FormEvent<HTMLFormElement>) {
+    event.preventDefault();
+    // TODO: send message to server
+  }
+
   return (
-    // TODO: wrap in <form> element
-    <div className="outline flex flex-row items-center space-x-4 p-4">
-      <textarea className="w-full outline"></textarea>
-      <button className="bg-slate-200 h-fit p-2">submit</button>
-    </div>
+    <form
+      className="outline flex flex-row items-center space-x-4 p-4"
+      onSubmit={onSubmit}
+    >
+      <textarea
+        className="w-full outline"
+        value={userMessage}
+        onChange={({ target }) => setUserMessage(target.value)}
+      ></textarea>
+      <input type="submit" value="submit" className="bg-slate-200 h-fit p-2" />
+    </form>
   );
 }
