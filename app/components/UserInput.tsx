b24829794244a62bc05d74b1a9f59765715f84fa
rey-soleil <68834278+rey-soleil@users.noreply.github.com>
2023-05-19 10:48:24 -0700

    feat(chat): add textarea, button for UserInput

--- app/components/UserInput.tsx
+++ app/components/UserInput.tsx
@@ -1,5 +1,11 @@
 "use client";
 
 export default function UserInput() {
-  return <></>;
+  return (
+    // TODO: wrap in <form> element
+    <div className="outline flex flex-row items-center space-x-4 p-4">
+      <textarea className="w-full outline"></textarea>
+      <button className="bg-slate-200 h-fit p-2">submit</button>
+    </div>
+  );
 }
