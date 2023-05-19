ea54455c3506c50d300981f6dc97d0098f9d0ce1
rey-soleil <68834278+rey-soleil@users.noreply.github.com>
2023-05-19 09:42:02 -0700

    style(home): add padding to header

--- app/page.tsx
+++ app/page.tsx
@@ -1,7 +1,10 @@
+import Messenger from "./components/Messenger";
+
 export default function Home() {
   return (
     <main className="flex flex-col items-center pt-20">
-      <h1 className="font-bold text-4xl">Writer&apos;s Blocks</h1>
+      <h1 className="font-bold text-4xl p-10">Writer&apos;s Blocks</h1>
+      <Messenger />
     </main>
   );
 }
